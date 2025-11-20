fd75b4c23a6903a389d23940f6fb02c3025dc51d
Nitramdesigner <your-email@example.com>
2025-11-20 16:04:58 -0500

    Simplify next config for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true,
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-          crypto: false,
-        }
-      }
-      config.externals.push('pino-pretty', 'lokijs', 'encoding', 'bufferutil', 'utf-8-validate')
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'react-native-sqlite-storage': false,
-      }
-      return config
-    },
-    transpilePackages: ['@rainbow-me/rainbowkit', 'wagmi', 'viem'],
-  }
-  
-  module.exports = nextConfig
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
